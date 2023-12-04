74175f81df893ab678d4bac4ba38e79cebbca1f6
aderemilekun <adelefatai@yahoo.com>
2023-12-04 14:07:03 +0100

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Aderemilekun Adele on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
