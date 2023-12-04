51c0f0b809b4419cb41503593c57febf61769301
aderemilekun <adelefatai@yahoo.com>
2023-12-04 16:22:28 +0100

    Added screens

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Aderemilekun Adele on 04/12/2023.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("MAp")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
