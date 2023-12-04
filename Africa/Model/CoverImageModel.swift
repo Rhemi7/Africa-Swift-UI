ab08d8fb60529dc6b565c29ef4218b60ac625571
aderemilekun <adelefatai@yahoo.com>
2023-12-04 17:06:49 +0100

    CoverImage model created

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Aderemilekun Adele on 04/12/2023.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
