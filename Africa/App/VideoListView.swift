1f0bf7f53c0ce46f25de4070ae7222fdbfe36b68
aderemilekun <adelefatai@yahoo.com>
2023-12-06 00:29:38 +0100

    Added haptic impact

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -13,6 +13,8 @@ struct VideoListView: View {
     
    @State var videos: [Video] = Bundle.main.decode("videos.json")
     
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
     // MARK: - BODY
     
     var body: some View {
@@ -30,6 +32,7 @@ struct VideoListView: View {
                     Button(action: {
                         // Shuffle videos
                         videos.shuffle()
+                        hapticImpact.impactOccurred()
                     }) {
                         Image(systemName: "arrow.2.squarepath")
                     }
