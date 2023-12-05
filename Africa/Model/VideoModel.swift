8d33f4075aabbbfd6448d2b4892fe45b84e96cc9
aderemilekun <adelefatai@yahoo.com>
2023-12-06 00:04:05 +0100

    Video model created

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Aderemilekun Adele on 06/12/2023.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
