b5b798dac819089518aee42267efb0145f1db6a8
aderemilekun <adelefatai@yahoo.com>
2023-12-06 00:24:22 +0100

    Created video list view

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    // MARK: - PROPERTIES
+    
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Video")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+        } //: NAVIGATION
     }
 }
 
