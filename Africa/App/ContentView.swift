aadccde5547eb31caac6ed921556289998a4fa56
aderemilekun <adelefatai@yahoo.com>
2023-12-07 17:19:05 +0100

    made grid button responsive with conditional statement

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -21,17 +21,23 @@ struct ContentView: View {
        
         
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    } //: LINK
-                } //: LOOP
-            } //: LIST
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: LINK
+                        } //: LOOP
+                    } //: LIST
+                } else {
+                    Text("grid view is active")
+                } //: CONDITION
+            } //: GROUP
             .navigationBarTitle("Africa", displayMode: .large)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by Aderemilekun Adele on 07/12/2023.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalGridItemView()
+    }
+}
