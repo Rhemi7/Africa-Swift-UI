2556745d84593ffd77ef3e4ccba6db4a4f019211
aderemilekun <adelefatai@yahoo.com>
2023-12-08 02:00:57 +0100

    Made credits view centralised

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
                         } //: LOOP
                         
                         CreditsView()
+                            .modifier(CenterModifier())
                     } //: LIST
                 } else {
                     ScrollView(.vertical, showsIndicators: false) {
